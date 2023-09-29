5a994269fb0054152dd3d60ea4d96e26a3002da6
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2023-09-30 00:34:39 +0300

    added the ability to remove employees from the list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,14 +8,33 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+          {name: 'Anna', salary: 500, increase: true, id: 1},
+          {name: 'Julia', salary: 1300, increase: false, id: 2},
+          {name: 'Kostya', salary: 6350, increase: true, id: 3},
+      ]
+    }
+  }
 
-  const data = [
-    {name: 'Anna', salary: 500, increase: true, id: 1},
-    {name: 'Julia', salary: 1300, increase: false, id: 2},
-    {name: 'Kostya', salary: 6350, increase: true, id: 3},
-  ];
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      // const index = data.findIndex(elem => elem.id === id);
+      // Вариант 1
+      // const before = data.slice(0, index);
+      // const after = data.slice(index + 1);
+      // const newArr = [...before, ...after]
+      // Вариант 2
+      return {
+       data: data.filter(item => item.id !== id)
+      }
+    })
+  }
 
+render() {
   return (
       <div className="app">
         <AppInfo/>
@@ -23,10 +44,14 @@ function App() {
           <AppFilter/>
         </div>
 
-        <EmployersList data={data}/>
+        <EmployersList 
+        data={this.state.data}
+        onDelete={this.deleteItem}
+        />
         <EmployersAddForm/>
       </div>
     )
+  }
 }
 
 export default App;
